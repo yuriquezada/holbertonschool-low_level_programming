a47a5213495420fdad8bbbf310b47af18003d456
Yuri Quezada <45071280+yuriquezada@users.noreply.github.com>
2021-06-18 16:30:55 -0500

    Add spaces; add description.
    Add spaces between the <and == symbols. Add description for main function.

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,9 +1,11 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <time.h>
-/* more headers goes there */
-
-/* betty style doc for function main goes there */
+/**
+ * main - Print whether the number stored in the variable n is positive, negative, or zero
+ *
+ * Return: Always 0 (Success)
+*/
 int main(void)
 {
 	int n;
@@ -11,9 +13,9 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 
-	if(n<0)
+	if (n < 0)
 		printf("%d is negative\n", n);
-	else if (n==0)
+	else if (n == 0)
 		printf("%d is zero\n", n);
 	else
 		printf("%d is positive\n", n);

ab94c6bac5ce301f3ab1d16d9eb8cee56ad58e00
Yuri Quezada <45071280+yuriquezada@users.noreply.github.com>
2021-06-18 17:35:51 -0500

    Add space between else and if

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -15,7 +15,7 @@ int main(void)
 
 	if (n%10 > 5)
 		printf("%d and is greater than 5\n", n);
-	elseif (n%10 == 0)
+	else if (n%10 == 0)
 		printf("%d and is 0\n", n);
 	else
 		printf("%d and is less than 6 and not 0\n", n);

6cb10844e3e8f9c38782b2d8d22b38e9743ab9e7
Yuri Quezada <45071280+yuriquezada@users.noreply.github.com>
2021-06-18 17:40:58 -0500

    Add first string and enclose the modulo
    Add first string. Enclose the modulo operation in parentheses

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -13,12 +13,12 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 
-	if (n%10 > 5)
-		printf("%d and is greater than 5\n", n);
-	else if (n%10 == 0)
-		printf("%d and is 0\n", n);
+	if ((n % 10) > 5)
+		printf("Last digit of %d and is greater than 5\n", n);
+	else if ((n % 10) == 0)
+		printf("Last digit of %d and is 0\n", n);
 	else
-		printf("%d and is less than 6 and not 0\n", n);
+		printf("Last digit of %d and is less than 6 and not 0\n", n);
 
 	return (0);
 }

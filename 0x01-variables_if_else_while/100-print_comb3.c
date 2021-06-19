d92c575cace254449516bf36a386a4c47862efe7
yuriquezada <quezadat.yuri@gmail.com>
2021-06-18 19:48:42 -0700

    Created 100-print_comb3.c file

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,28 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * main - Print prints all possible different combinations of two digits
+ *
+ * Return: 0
+*/
+int main(void)
+{
+	int d;
+	int u;
+
+	for (d = 48; d <= 57; d++)
+	{
+		for (u=d+1; u<=57; u++)
+		{
+			putchar(d);
+			putchar(u);
+			if (n < 57)
+			{
+				putchar(44);
+				putchar(32);
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}

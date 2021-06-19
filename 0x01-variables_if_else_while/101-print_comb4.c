61c55065a8161c2e5d9066d6ef384cbfd9f44226
yuriquezada <quezadat.yuri@gmail.com>
2021-06-18 20:07:01 -0700

    Create 101-print_comb4.c file

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,34 @@
+  
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * main - Print all possible different combinations of three digits
+ *
+ * Return: 0
+*/
+int main(void)
+{
+	int c;
+	int d;
+	int u;
+
+	for (c = 48; c <= 55; d++)
+	{
+		for (d = d + 1; d <= 56; d++)
+		{
+			for (u = d + 1; u <= 57; u++)
+			{
+				putchar(c);
+				putchar(d);
+				putchar(u);
+				if (c < 55)
+				{
+					putchar(44);
+					putchar(32);
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}

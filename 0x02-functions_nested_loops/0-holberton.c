0900862bffd6917435b673c9602415ae32a2c762
Yuri Quezada <45071280+yuriquezada@users.noreply.github.com>
2021-06-22 15:18:51 -0500

    Declarate variable i in 11 line

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -5,10 +5,12 @@
  *
  * Return: 0
 */
+
 int main(void)
 {
+	int i;
 	char p[] = "Holberton";
-	for (int i = 0; i < 9; i++)
+	for (i = 0; i < 9; i++)
 		_putchar(p[i]);
 	_putchar('\n');
 	return (0);

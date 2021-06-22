368e5c160c1a7aeadf1a2cad2c5f6ff7025aaaef
yuriquezada <quezadat.yuri@gmail.com>
2021-06-22 16:48:52 -0700

    Remove the spaces before the tab in row 12

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,15 +1,15 @@
 #include "holberton.h"
 
 /**
-* print_alphabet - Print 10 times the alphabet in lowercase.
-*
-* Return: void.
+ * print_alphabet - Print 10 times the alphabet in lowercase.
+ *
+ * Return: void.
 */
 
 void print_alphabet_x10(void)
 {
 	int letter;
-  	int time;
+	int time;
 	for (time = 0; time < 10; time++)
 	{
 		for (letter = 97; letter < 123; letter++)

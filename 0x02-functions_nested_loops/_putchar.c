fff281f6b102432196d0191d475fcd07cc1f04ee
yuriquezada <quezadat.yuri@gmail.com>
2021-06-22 11:55:32 -0700

    Create _putchar.c file

---
+++ 0x02-functions_nested_loops/_putchar.c
@@ -0,0 +1,13 @@
+#include 
+
+/ **
+ * _putchar - escribe el carácter c en stdout
+ * @c: el carácter que se va a imprimir
+ *
+ * Devolución: en caso de éxito 1.
+ * En caso de error, se devuelve -1 y errno se establece correctamente.
+ * /
+int _putchar (carácter c)
+{
+return (escribir (1, & c, 1));
+}

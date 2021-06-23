342ec3970e7e6a0e0cf48227973a00235fec5bff
yuriquezada <quezadat.yuri@gmail.com>
2021-06-22 21:46:54 -0700

    Create 5-sign.c file

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,28 @@
+#include "holberton.h"
+
+/**
+ * print_sign - Print the sign of a number
+ *
+ * @n: number
+ *
+ * Return: -1, 0 or 1
+*/
+
+int print_sign(int n)
+{
+	if (n < 0)
+	{
+		putchar('-');
+		return (-1);
+	}
+	else if (n == 0)
+	{
+		putchar('0');
+		return (0);
+	}
+	else
+	{
+		putchar('+');
+		return (1);
+	}
+}

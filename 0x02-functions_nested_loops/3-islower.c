862f699aca5112c1d921c8a06c2e646beca32757
Yuri Quezada <45071280+yuriquezada@users.noreply.github.com>
2021-06-22 21:38:58 -0500

    Update 3-islower.c

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -3,18 +3,19 @@
 /**
  * _islower - Check for lowercase character
  *
- * Description: Check for lowercase character. Return 1 if c is lowercase, return 0 otherwise
+ * Description: Check for lowercase character.
  *
- * Return: void
+ * Return: 0 or 1
 */
 
-int _islower(int c){
+int _islower(int c)
+{
 	if (c >= 97 && c <= 122)
 	{
-		return 1;
+		return (1);
 	}
 	else
 	{
-		return 0;
+		return (0);
 	}
 }

97ac26609f8e43c339cd7bd055e68badee323423
yuriquezada <quezadat.yuri@gmail.com>
2021-06-22 21:08:33 -0700

    Create 4-isalpha.c file

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+
+/**
+ * _isalpha - Check for lowercase and uppercase character
+ *
+ * @c: character to check
+ *
+ * Return: 0 or 1
+*/
+
+int _isalpha(int c){
+	if ((c >= 65 && c <= 90) || (c >= 97 && c <= 122))
+		return 1;
+	else
+		return 0;
+}

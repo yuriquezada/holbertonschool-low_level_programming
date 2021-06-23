8eb48f9a1106d8add7d31df37da9b1f09b641530
Yuri Quezada <45071280+yuriquezada@users.noreply.github.com>
2021-06-22 23:16:46 -0500

    Correct about Betty coding style

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -8,9 +8,9 @@
  * Return: 0 or 1
 */
 
-int _isalpha(int c){
+int _isalpha(int c) {
 	if ((c >= 65 && c <= 90) || (c >= 97 && c <= 122))
-		return 1;
+		return (1);
 	else
-		return 0;
+		return (0);
 }

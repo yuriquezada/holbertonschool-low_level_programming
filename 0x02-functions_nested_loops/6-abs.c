16747fe547b9517f1d408bcaee1e0d352cb12269
yuriquezada <quezadat.yuri@gmail.com>
2021-06-22 22:00:33 -0700

    Create 6-abs.c file

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+
+/**
+ * _abs - Output absolut value of an integer
+ * @n: Number to check
+ *
+ * Return: n
+ */
+
+
+int _abs(int n)
+{
+	if (n < 0)
+	{
+		n *= -1;
+		return (n);
+
+	else
+		return (n);
+}

6b3399c6ffe57cdd04be9dcac2d93d206c817161
yuriquezada <quezadat.yuri@gmail.com>
2021-06-22 23:24:13 -0700

    Create -m 8-24_hours.c file

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,35 @@
+#include "holberton.h"
+
+/**
+ * jack_bauer - Print every minute from 00:00 to 23:59
+ *
+ * Description: Print every minute of the day of Jack Bauer
+ * 
+ * Return: 0
+ */
+
+
+void jack_bauer(void)
+{
+	int a, b, p, q;
+
+	for (a = '0'; a <= '2'; a++)
+	{
+		for (b = '0'; b <= '3'; b++)
+		{
+			for (p = '0'; p <= '5'; p++)
+			{
+				for (q = '0'; q <= '9'; q++)
+				{
+					putchar(a);
+					putchar(b);
+					putchar(':');
+					putchar(p);
+					putchar(q);
+					putchar('\n');
+				}
+			}      
+		}
+	}
+	return (0);
+}

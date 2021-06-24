b0392e4ec55ba4a5ae7eb2382f01e4bc31a8781c
yuriquezada <quezadat.yuri@gmail.com>
2021-06-24 07:44:15 -0700

    Create 0-isupper.c file

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+
+/**
+ * _isupper - Check for uppercase character
+ *
+ * @c: character to check
+ *
+ * Return: 0 or 1
+*/
+
+int _isupper(int c)
+{
+	if (c >= 65 && c <= 90)
+		return (1);
+	else
+		return (0);
+}

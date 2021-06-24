432a63bad9e6b9808fe50001458e083454ee450e
yuriquezada <quezadat.yuri@gmail.com>
2021-06-23 20:45:24 -0700

    Create 10-add.c file

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+
+/**
+ * add - Add two integers and returns the result
+ *
+ * @num1: First operand
+ * @num2: Second operand
+ *
+ * Return: The sum of the two parameters
+ */
+
+
+int add(int num1, int num2)
+{
+	int result, tens, ones;
+
+	result = num1 + num2;
+	tens = result / 10;
+	ones = number % 10;
+	_putchar(tens + 48);
+	_putchar(ones + 48);
+	_putchar('\n');
+}

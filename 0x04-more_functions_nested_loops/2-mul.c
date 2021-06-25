12e759d05955863060ccbd5d06f098d7e34694de
yuriquezada <quezadat.yuri@gmail.com>
2021-06-24 21:16:02 -0700

    Create 2-mul.c file

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+
+/**
+ * mul - Multiplie two integers
+ *
+ * @a: First operand
+ * @b: Second operand
+ *
+ * Return: product
+*/
+
+int mul(int a, int b)
+{
+	return a * b;
+}

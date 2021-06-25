b24bd66c25a9dbca83d56317e7558b3c42b52a3b
yuriquezada <quezadat.yuri@gmail.com>
2021-06-25 10:27:43 -0700

    Create 6-print_line file

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+
+/**
+ * print_line - Draws a straight line in the terminal
+ * @n: Number of times the character _
+ * Return: void
+*/
+void print_line(int n);
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+		_putchar('_');
+	_putchar('\n');
+}

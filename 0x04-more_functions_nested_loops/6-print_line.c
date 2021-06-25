21161cbbcf35f8f342eaf0f7cdf8415c3041bd3e
Yuri Quezada <45071280+yuriquezada@users.noreply.github.com>
2021-06-25 12:40:56 -0500

    Update 6-print_line.c

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -9,12 +9,8 @@ void print_line(int n);
 {
 	char i;
 
-	if (n <= 0)
-		_putchar('\n');
-	else
-	{
+	if (n > 0)
 		for (i = 0; i < n; i++)
 			_putchar('_');
-		_putchar('\n');
-	}
+	_putchar('\n');
 }

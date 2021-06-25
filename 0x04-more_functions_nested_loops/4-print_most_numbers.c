9a87f946272cc4c1cc81b9fbd077e5aa593176b8
yuriquezada <quezadat.yuri@gmail.com>
2021-06-25 09:44:20 -0700

    Create 4-print_most_numbers.c file

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+
+/**
+ * print_numbers - Print the numbers from 0 to 9, except 2 and 4
+ *
+ * Return: void
+*/
+void print_numbers(void)
+{
+	char n;
+
+	for (n = '0'; n <= '9'; n++)
+		if (n != 2 && n != 4)
+			_putchar(n);
+	_putchar('\n');
+	return (0);
+}

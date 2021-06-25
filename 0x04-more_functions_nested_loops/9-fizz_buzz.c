feaca5a0bafd4f41c75597ae705bdc001db4fa37
Yuri Quezada <45071280+yuriquezada@users.noreply.github.com>
2021-06-25 16:18:36 -0500

    Delete space in result

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,4 +1,3 @@
-#include "holberton.h"
 #include <stdio.h>
 
 /**
@@ -11,7 +10,7 @@ void fizz_buzz(void)
 {
 	int i;
 
-	for (i = 1; i <= 100; i++)
+	for (i = 1; i < 100; i++)
 	{
 		if (i % 15 == 0)
 			printf("FizzBuzz");
@@ -21,7 +20,8 @@ void fizz_buzz(void)
 			printf("Fizz");
 		else
 			printf("%d",i);
-		_putchar(' ');
+		printf(' ');
 	}
-	_putchar('\n');
+	printf("Buzz");
+	printf("\n");
 }

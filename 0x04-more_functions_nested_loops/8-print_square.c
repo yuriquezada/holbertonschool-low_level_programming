28fba13bfe02f0e889bbd03b8c9666532148ecfe
Yuri Quezada <45071280+yuriquezada@users.noreply.github.com>
2021-06-25 15:26:38 -0500

    Change 'n' to 'size'

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -2,18 +2,18 @@
 
 /**
  * print_square - Print a square followed by a new line
- * @n: Number of times the character '_'
+ * @size: Number of times the character '_'
  * Return: void
 */
 
-void print_square(int n)
+void print_square(int size)
 {
 	int i, m;
 
-	if (n > 0)
-		for (i = 0; i < n; i++)
+	if (size > 0)
+		for (i = 0; i < size; i++)
 		{
-			for (m = 0; m < n; m++)
+			for (m = 0; m < size; m++)
 				_putchar('#');
 			_putchar('\n');
 		}

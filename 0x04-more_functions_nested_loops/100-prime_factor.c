459f041686ca81ea90674f032548dc574b31fd63
yuriquezada <quezadat.yuri@gmail.com>
2021-06-25 21:55:24 -0700

    Create 100-prime_factor.c file

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Find and print the largest prime factor
+ *
+ * Return: 0
+*/
+
+int main(void)
+{
+	int i, number = 612852475143;
+	
+	for (i = 2; i < number / 2 + 1; i++)
+		if (number % i == 0)
+		{
+			number /= i;
+			i--;
+		}
+	printf(number);
+	printf("\n");
+	return (0);
+}

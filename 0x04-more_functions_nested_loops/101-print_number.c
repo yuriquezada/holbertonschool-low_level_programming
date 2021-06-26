8720793b05be63c7032690047bfa1651ed72f20b
yuriquezada <quezadat.yuri@gmail.com>
2021-06-26 14:57:50 -0700

    Create 101-print_number.c file

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,29 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <math.h>
+
+/**
+ * print_number - Prints an integer
+ * @n: Integer to print
+ * Return: 0
+*/
+
+void print_number(int n)
+{
+	int i, power = 0, digit;
+	
+	if (n < 0)
+	{
+		n *= -1;
+		_putchar('-');
+	}
+	for (i = 10; i <= n; i *= 10);
+		power++;
+	while(power >= 0)
+	{
+		digit = (int)(n / pow(10, power)) % 10;
+		putchar('0' + digit);
+		power--;
+	}
+	return (0);
+}

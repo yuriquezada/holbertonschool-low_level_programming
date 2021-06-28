b25cbb661c41eea9981d441045c8c4c4f856160f
yuriquezada <quezadat.yuri@gmail.com>
2021-06-28 12:31:19 -0700

    Create reset_to_98.c file

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+
+/**
+ * reset_to_98 - Updates the value it points to to 98
+ *
+ * Return: void
+*/
+void reset_to_98(int *n)
+{
+	int f, n;
+
+	n = &98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+In this project we will learn about pointers in C.

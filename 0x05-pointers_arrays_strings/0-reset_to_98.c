0f42b5fec47eda231337700ac28089436395846d
Yuri Quezada <45071280+yuriquezada@users.noreply.github.com>
2021-06-28 15:01:18 -0500

    Delete '*' in parameter 'n'

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -2,7 +2,7 @@
 
 /**
  * reset_to_98 - Updates the value it points to to 98
- * @*n: Pointer to which we will assign a value
+ * @n: Pointer to which we will assign a value
  * Return: void
 */
 void reset_to_98(int *n)

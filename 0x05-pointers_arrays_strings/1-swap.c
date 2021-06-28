a6b33ebfea00ab4cc7bb88fac0c09e3768beb6c2
yuriquezada <quezadat.yuri@gmail.com>
2021-06-28 13:23:11 -0700

    Create 1-swap.c file

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+
+/**
+ * swap_int - Swap the values of two integers
+ * @a: First integer
+ * @b: Second integer
+ * Return: void
+*/
+void reset_to_98(int *a, *b)
+{
+	*a = b;
+	*b = a;
+}

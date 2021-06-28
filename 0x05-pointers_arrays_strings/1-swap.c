be44b19891bf655f0e468eb8b2cbf0f9b903d741
Yuri Quezada <45071280+yuriquezada@users.noreply.github.com>
2021-06-28 15:40:49 -0500

    Update 1-swap.c

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -8,8 +8,8 @@
 */
 void swap_int(int *a, int *b)
 {
-	int *fistPointer = *a;
-	int *secondPointer = *b;
+	int *fistPointer = a;
+	int *secondPointer = b;
 	*a = *secondPointer;
 	*b = *fistPointer;
 }

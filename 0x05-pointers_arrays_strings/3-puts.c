aad989d437fdb8cceb4f684ddda0454549fd673b
yuriquezada <quezadat.yuri@gmail.com>
2021-06-28 20:37:01 -0700

    Create 3-puts.c file

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+
+/**
+ * _puts - Prints a string followed by a new line
+ * @str: First integer
+ * Return: void
+*/
+void _puts(char *str)
+{
+	while (*s != '\0')
+	{
+		printf("%c\n", *s);
+		s++;
+	}
+	printf("\n");
+}

4ced035f794def6ec4948d80b5ebefccc4633de7
yuriquezada <quezadat.yuri@gmail.com>
2021-06-30 07:10:40 -0700

    Create 7-puts_half.c file

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,29 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+ * puts_half - Print every other character of a string
+ *
+ * @str: String to print
+ *
+ * Return: void
+*/
+
+void puts_half(char *str)
+{
+	int stringLength = 0, i;
+	char *newPointer = str;
+
+	while (*newPointer != '\0')
+	{
+		stringLength++;
+		newPointer++;
+	}
+
+	if (stringLength % 2 == 1)
+		i = (stringLength + 1) / 2;
+	else
+		i = stringLength / 2;
+	for (; i < stringLength; i ++)
+		printf("%c", str[i]);
+	printf("\n");
+}

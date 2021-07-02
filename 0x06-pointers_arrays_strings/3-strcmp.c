251a6f152886c8a816e2d60efca1e23de5ddac4f
yuriquezada <quezadat.yuri@gmail.com>
2021-07-01 22:07:56 -0700

    Create 3-strcmp.c file

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,28 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * _strcmp - function that compares two strings
+ *
+ * @s1: string
+ * @s2: string
+ *
+ * Return: result
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	int lenS1 = 0, lenS2 = 0, i;
+	int result;
+	while (*(s1 + lenS1) != '\0')
+		lenS1++;
+	while (*(s2 + lenS2) != '\0')
+		lenS2++;
+	for (i = 0; i < lenS1; i++)
+	{
+		result = s1[i] - s2[i];
+		if (s1[i] != s2[i])
+		    break;
+	}
+	return (result);
+}

36ac3648fab60389f186a90088800e5ccdc7f755
yuriquezada <quezadat.yuri@gmail.com>
2021-07-01 18:29:31 -0700

    Create 0-strcat.c file

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * main - concatenates two strings
+ *
+ * @dest: string to the dest
+ * @src: string to append
+ *
+ * Return: dest
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	int lenSrc = 0, lenDest = 0, i;
+
+	while (*(dest + lenDest) != '\0')
+		lenDest++;
+	while (*(src + lenSrc) != '\0')
+		lenSrc++;
+	for (i = 0; i < lenSrc; i++)
+		dest[i + lenDest] = src[i];
+	return (dest);
+}

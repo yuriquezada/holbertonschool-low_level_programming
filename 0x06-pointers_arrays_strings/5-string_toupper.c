0ce4a1390c05c3ae1ca0355815af67ca80a9279a
yuriquezada <quezadat.yuri@gmail.com>
2021-07-01 23:18:26 -0700

    Create 5-string_toupper.c file

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * string_toupper - Change all lowercase letters to uppercase
+ *
+ * @a: array of char
+ *
+ * Return: a
+ */
+
+char *string_toupper(char *a)
+{
+    // char *a = str;
+	int i, len =0;
+
+	while (*(a + len) != '\0')
+		len++;
+	for (i = 0; i < len; i++)
+	{
+        if (a[i] >= 'a' && a[i] <= 'z')
+            a[i] -= 32;
+	}
+	return (a);
+}

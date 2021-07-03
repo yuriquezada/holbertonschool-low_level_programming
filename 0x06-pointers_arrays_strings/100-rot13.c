c70b632ff90b9edd5e63bd5a45d3feaabde01f82
yuriquezada <quezadat.yuri@gmail.com>
2021-07-02 21:24:42 -0700

    Create 100-rot13.c file

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,28 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * rot13 - Encodes a string using rot13
+ *
+ * @a: array of char
+ *
+ * Return: a
+ */
+
+char *rot13(char *a)
+{
+	int lenS = 0, i;
+
+	while (*(a + lenS) != '\0')
+		lenS++;
+	for (i = 0; i < lenS; i++)	
+	{
+		if ((a[i] >= 'N' && a[i] <= 'Z') || (a[i] >= 'n' && a[i] <= 'z'))
+			a[i] -= 13;
+		else if ((a[i] >= 'A' && a[i] <= 'M') || (a[i] >= 'a' && a[i] <= 'z'))
+			a[i] += 13;
+		else
+			a[i] = a[i];
+	}
+	return (a);
+}

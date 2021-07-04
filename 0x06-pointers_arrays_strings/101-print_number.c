dd40256efbfe22dc4c5e0540d0773935715d2f7f
yuriquezada <quezadat.yuri@gmail.com>
2021-07-03 23:01:43 -0700

    Create 101-print_number.c file

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,26 @@
+#include "holberton.h"
+
+/**
+ * print_number - Prints an integer using _putchar function
+ * @n: Integer to print
+ * Return: void
+*/
+
+void print_number(int n)
+{
+	int power10 = 1, digit;
+
+	if (n < 0)
+	{
+		n *= -1;
+		_putchar('-');
+	}
+	while (power10 * 10 <= n)
+		power10 *= 10;
+	while (power10 > 0)
+	{
+		digit = (n / power10) % 10;
+		power10 /= 10;
+		_putchar('0' + digit);
+	}
+

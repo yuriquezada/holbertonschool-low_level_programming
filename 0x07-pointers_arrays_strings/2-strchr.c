a13a3c49c931aad1a8fcebe4e482c435a29a4b81
yuriquezada <quezadat.yuri@gmail.com>
2021-07-05 22:49:14 -0700

    Create 2-strchr.c file

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+/**
+ *_strchr - Locate a character in a string
+ *@s: string
+ *@c: character in the string
+ *
+ *Return: &s[i] or 0
+ */
+char *_strchr(char *s, char c)
+{
+	int i = 0;
+
+	for (i = 0; s[i] != '\0' ; i++)
+		if (s[i] == c)
+			return (&s[i]);
+	return (0);
+}

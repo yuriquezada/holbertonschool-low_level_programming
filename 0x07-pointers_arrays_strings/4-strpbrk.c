a9f3c0ea01381b2b020a2e280ab83cfa5b91384c
yuriquezada <quezadat.yuri@gmail.com>
2021-07-06 21:28:55 -0700

    Create 4-strpbrk.c file

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+/**
+ *_strspn - search a string for any of a set of bytes
+ *@s: substring
+ *@accept: initial string
+ *
+ *Return: number
+ */
+
+char *_strpbrk(char *s, char *accept)
+{
+	int i, j, c;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		c = 0;
+		for (j = 0; accept[j] != '\0'; j++)
+			if (s[i] == accept[j])
+				c = 1;
+		if (c == 1)
+			return (&s[i]);
+	}
+	if (c == 0)
+		return (0);
+}

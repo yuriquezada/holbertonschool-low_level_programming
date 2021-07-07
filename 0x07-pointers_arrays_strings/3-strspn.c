a181f4856842c47ce646ed0e898d504c7f4cdca0
Yuri Quezada <45071280+yuriquezada@users.noreply.github.com>
2021-07-06 23:03:11 -0500

    Change indexes and add '=' in if block

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -9,17 +9,17 @@
 
 unsigned int _strspn(char *s, char *accept)
 {
-	int i = 0, j = 0, c = 0;
+	int i, j, c;
 	unsigned int number = 0;
 
-	for (; s[i] != '\0' ; i++)
+	for (i = 0; s[i] != '\0'; i++)
 	{
 		c = 0;
-		for (; accept[j] != '\0'; j++)
-			if (s[i] = accept[j])
+		for (j = 0; accept[j] != '\0'; j++)
+			if (s[i] == accept[j])
 				c = 1;
 		if (c == 1)
-			number += c;
+			number ++;
 		else
 			break;
 	}

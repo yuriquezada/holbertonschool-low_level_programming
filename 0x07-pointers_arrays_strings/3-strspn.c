dca792e0a36d5039ec77fdae600157834c706374
yuriquezada <quezadat.yuri@gmail.com>
2021-07-06 20:20:45 -0700

    Create 3-strspn.c file

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,27 @@
+#include "holberton.h"
+/**
+ *_strspn - get the length of a prefix substring
+ *@s: substring
+ *@accept: initial string
+ *
+ *Return: number
+ */
+
+unsigned int _strspn(char *s, char *accept)
+{
+	int i = 0, j = 0, c = 0;
+	unsigned int number = 0;
+
+	for (; s[i] != '\0' ; i++)
+	{
+		c = 0;
+		for (; accept[j] != '\0'; j++)
+		if (s[i] = accept[j])
+			c = 1;
+		if (c == 1)
+			number += c;
+		else
+			break;
+	}
+	return (number);
+}

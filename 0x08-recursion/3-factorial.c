29dc1acc1a471208e4fe35a0bc52ae0f14e36d94
yuriquezada <quezadat.yuri@gmail.com>
2021-07-07 20:50:33 -0700

    Create file

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * factorial - Return the factorial of a given number
+ *
+ * @n: number
+ *
+ * Return: factorial
+ */
+
+int factorial(int n)
+{
+	if (n < 0)
+		return (-1);
+	if (n == 0)
+		return (1);
+	return (factorial(n - 1) * n);
+}
+

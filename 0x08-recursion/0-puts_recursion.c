4152c7a74d70a0bfa44ae5ad0134952bad960f7e
yuriquezada <quezadat.yuri@gmail.com>
2021-07-07 18:54:19 -0700

    Create 0-puts_recursion.c file

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * _puts_recursion - print a string, followed by a new line
+ *
+ * @s: string
+ *
+ * Return: void
+ */
+
+void _puts_recursion(char *s)
+{
+	int i = 0;
+
+	printf("%c", s[i]);
+	s++;
+	if (s[i] != '\0')
+		_puts_recursion(s);
+	else
+		printf("\n");
+}

6cf29abf90a7694bdf7d747e5cfbd4184acd607c
Yuri Quezada <45071280+yuriquezada@users.noreply.github.com>
2021-07-09 11:29:33 -0500

    Add _strlen_recursion function

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,13 +1,27 @@
 #include "holberton.h"
 
 /**
- * is_palindrome - Return if a string is a palindrome
+ * _strlen_recursion - Return the length of a string
  *
  * @s: string
  *
+ * Return: length of a string
+ */
+int _strlen_recursion(char *s)
+{
+	if (*s == '\0')
+		return (0);
+	return (_strlen_recursion(s + 1) + 1);
+}
+
+/**
+ * find_letter - Return if a string is a palindrome
+ *
+ * @s: string
+ * @len: string length
+ *
  * Return: 1 or 0
  */
-
 int find_letter(char *s, int len)
 {
 	if (len == 1 || len == 0)
@@ -17,6 +31,13 @@ int find_letter(char *s, int len)
 	return (find_letter(s + 1, len - 2));
 }
 
+/**
+ * is_palindrome - Return if a string is a palindrome
+ *
+ * @s: string
+ *
+ * Return: 1 or 0
+ */
 int is_palindrome(char *s)
 {
 	int len;

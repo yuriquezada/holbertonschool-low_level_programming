bd16f8b2feb7c099186fcf1fa5ed46a82bd93300
Yuri Quezada <45071280+yuriquezada@users.noreply.github.com>
2021-07-13 20:49:48 -0500

    Update 0-whatsmyname.c

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
+
 /**
  * main -  writes its name
  * @argc: arguement count
  * @argv: array of pointers to arguement strings
  * Return: 0
  **/
+
 int main(int argc, char *argv[])
 {
 	argc = argc;

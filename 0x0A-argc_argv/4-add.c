8abda757b3178760eee21cf3fa872e2a7bf32401
yuriquezada <quezadat.yuri@gmail.com>
2021-07-13 22:26:16 -0700

    Create 4-add.c file

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Add positive numbers
+ * @argc: Arguement count
+ * @argv: Array of pointers to arguement strings
+ * Return: 0 or 1
+ **/
+
+int main(int argc, char *argv[])
+{
+	int i, j, sum = 0, invalid = 0;
+
+	argc = argc;
+	if (argc == 1)
+	{
+		printf("0\n");
+		return (0);
+	}
+	for (i = 1; i < argc; i++)
+	{
+		for (j = 0; argv[i][j] != '\0'; j++)
+			if (!((argv[i][j] >= '0') && (argv[i][j] <= '9')))
+			{
+				invalid = 1;
+				break;
+			}
+		if (!invalid)
+			sum += atoi(argv[i]);
+	}
+	if (invalid)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	printf("%d\n", sum);
+	return (0);
+}

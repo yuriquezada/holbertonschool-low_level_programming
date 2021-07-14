18d7f20b9946a67a07878ab1a3f9a10ce0b2b005
yuriquezada <quezadat.yuri@gmail.com>
2021-07-13 19:37:59 -0700

    Create 3-mul.c file

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Multiplies two numbers
+ * @argc: Arguement count
+ * @argv: Array of pointers to arguement strings
+ * Return: 0 or 1
+ **/
+
+int main(int argc, char *argv[])
+{
+	int a, b;
+
+	argc = argc;
+	if (argc != 3)
+		printf("Error\n");
+		return (1);
+	a = atoi(argv[1]);
+	b = atoi(argv[2]);
+	printf("%d", a * b);
+	return (0);
+}

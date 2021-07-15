8bc5ed8de3f0178503a7854a748d31d15c3b7b73
Yuri Quezada <45071280+yuriquezada@users.noreply.github.com>
2021-07-15 17:47:39 -0500

    Update 1-string_nconcat.c

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -20,7 +20,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	for (i = 0; s1[i] != '\0'; i++)
 		len1++;
 	p = malloc(len1 + n + 1);
-	if (strDup == NULL)
+	if (p == NULL)
 		return (NULL);
 	for (i = 0; i < len1; i++)
 		p[i] = s1[i];

d218deece7a24d6e51da8cbdaaf011a75b3d9305
Yuri Quezada <45071280+yuriquezada@users.noreply.github.com>
2021-07-15 20:47:45 -0500

    Update 2-calloc.c

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -19,6 +19,6 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	if (p == NULL)
 		return (NULL);
 	for (i = 0; i < (nmemb * size); i++)
-		p[i] = 0;
+		*(p + i) = 0;
 	return ((void *)p);
 }

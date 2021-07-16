eb828aad1d8b9f3548f715cff467a23fccfe92be
Yuri Quezada <45071280+yuriquezada@users.noreply.github.com>
2021-07-15 20:11:27 -0500

    Add ')' in line 21

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -14,11 +14,11 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	unsigned int i;
 
 	if (memb == 0 || size == 0)
-        return (NULL);
+        	return (NULL);
 	p = malloc(nmemb * size);
 	if (p == NULL)
 		return (NULL);
-	for (i = 0; i < (nmemb * size; i++))
+	for (i = 0; i < (nmemb * size); i++))
 		p[i] = 0;
 	return (p);
 }

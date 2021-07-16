3db798928006b97c99fdbbcaab35178a3151aadd
Yuri Quezada <45071280+yuriquezada@users.noreply.github.com>
2021-07-15 22:51:33 -0500

    Change *p with int

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -15,7 +15,7 @@ int *array_range(int min, int max)
 
 	if (min > max)
 		return (NULL);
-	p = malloc(sizeof(*p) * (max - min + 1));
+	p = malloc(sizeof(int) * (max - min + 1));
 	if (p == NULL)
 		return (NULL);
 	for (i = 0; i < (max - min + 1); i++, min++)

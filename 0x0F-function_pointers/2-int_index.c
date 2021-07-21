800f0b57165f47d2c78fea00b1addc711ebbf569
Yuri Quezada <45071280+yuriquezada@users.noreply.github.com>
2021-07-21 15:41:35 -0500

    Refactor function

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -12,16 +12,12 @@ int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i;
 
-	if (array && cmp)
-	{
-		if (size <= 0)
-			return (-1);
+	if (size > 0 && array && cmp)
 		for (i = 0; i < size; i++)
 			if (cmp(array[i]))
 			{
 				return (i);
 				break;
 			}
-	}
 	return (-1);
 }

412372c3d827d3e323de38ad9199d5c4ee625265
Yuri Quezada <45071280+yuriquezada@users.noreply.github.com>
2021-07-21 15:29:23 -0500

    Add condition if pointers are not null

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -12,7 +12,7 @@ int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i;
 
-	if (array != NULL && cmp != NULL)
+	if (array && cmp)
 		for (i = 0; i < size; i++)
 			if (cmp(array[i]))
 				break;

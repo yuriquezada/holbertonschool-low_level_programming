b2acb0cee95247cc937a230789e376abb2b30edb
yuriquezada <quezadat.yuri@gmail.com>
2021-07-22 11:53:24 -0700

    Create 1-print_numbers.c file

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,25 @@
+#include "variadic_functions.h"
+#include <stdarg.h>
+#include <stdio.h>
+
+/**
+ * print_numbers - print numbers, followed by a new line
+ * @n: string to be printed between numbers
+ * @n: number of arguments to concatenate
+ * Return: sum or 0
+ */
+
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	va_list ap;
+	unsigned int i, sum = 0;
+	va_start(ap, n);
+	for (i = 0; i < n; i++)
+	{
+		if (i)
+			printf(", ");
+		printf("%d", va_arg(ap, int));
+	}
+	printf("\n");
+	va_end(ap);
+}

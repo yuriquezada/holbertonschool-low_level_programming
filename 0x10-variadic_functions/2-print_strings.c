25bd4b3efdaef5651e3554c272c64a3974db6757
Yuri Quezada <45071280+yuriquezada@users.noreply.github.com>
2021-07-22 17:09:55 -0500

    Delete space in function

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -22,7 +22,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
  		if (i && separator)
 			printf("%s", separator);
 		if (str)
- 			printf("%s", str);  
+			printf("%s", str);  
 		else
 			printf("(nil)");
 	}

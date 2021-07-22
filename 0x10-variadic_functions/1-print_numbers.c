cb1addef45741b369ab06c918430d8e3be815dad
Yuri Quezada <45071280+yuriquezada@users.noreply.github.com>
2021-07-22 14:08:04 -0500

    Change line breaks in variable declaration

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -13,8 +13,8 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 {
 	va_list ap;
 	unsigned int i;
-	va_start(ap, n);
 
+	va_start(ap, n);
 	for (i = 0; i < n; i++)
 	{
 		if (i && separator)

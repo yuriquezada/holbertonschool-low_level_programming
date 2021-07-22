9ed9d3bb342646b4941eb5cb1cda45393378cd0e
yuriquezada <quezadat.yuri@gmail.com>
2021-07-22 10:19:01 -0700

    Create 0-sum_them_all.c file

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,23 @@
+#include "variadic_functions.h"
+#include <stdarg.h>
+#include <stdio.h>
+
+/**
+ * main - check the code for Holberton School students.
+ * @n: number of arguments to add
+ * Return: sum or 0
+ */
+
+int sum_them_all(const unsigned int n, ...)
+{
+	va_list ap;
+	int i, sum = 0;
+	va_start (ap, n);
+
+	if (n == 0)
+		return (0);
+	for (i = 0; i < n; i++)
+		sum += va_arg (ap, int);
+	va_end (ap);
+	return (sum);
+}

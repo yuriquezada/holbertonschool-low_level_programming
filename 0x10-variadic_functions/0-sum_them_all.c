eb345786af4806330d291510c97f47837f8de1bc
Yuri Quezada <45071280+yuriquezada@users.noreply.github.com>
2021-07-22 12:40:17 -0500

    Change location of va_start

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -12,10 +12,10 @@ int sum_them_all(const unsigned int n, ...)
 	unsigned int i;
 	int sum = 0;
 	va_list ap;
-	va_start (ap, n);
 
 	if (n == 0)
 		return (0);
+	va_start (ap, n);
 	for (i = 0; i < n; i++)
 		sum += va_arg (ap, int);
 	va_end (ap);

4c3f46cb0def1b5a539dc4cd062071d28bc2189f
Yuri Quezada <45071280+yuriquezada@users.noreply.github.com>
2021-07-22 12:34:56 -0500

    Add 'unsigned int' in sum

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -11,13 +11,13 @@
 int sum_them_all(const unsigned int n, ...)
 {
 	unsigned int i, sum = 0;
-
 	va_list ap;
 	va_start (ap, n);
+
 	if (n == 0)
 		return (0);
 	for (i = 0; i < n; i++)
-		sum += va_arg (ap, int);
+		sum += va_arg (ap, unsigned int);
 	va_end (ap);
 	return (sum);
 }

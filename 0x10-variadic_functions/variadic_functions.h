17894ba934554af1ee632bbe32160c62bca333b3
Yuri Quezada <45071280+yuriquezada@users.noreply.github.com>
2021-07-22 23:28:43 -0500

    Add print structure in file

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,6 +1,19 @@
 #ifndef VARIADIC_FUNCTIONS_H
 #define VARIADIC_FUNCTIONS_H
 
+#include <stdarg.h>
+/**
+ * struct print - print type with corresponding print function
+ * @t: print type
+ * @f: print function
+ */
+typedef struct print
+{
+	char *c;
+	void (*f)(va_list);
+} print_based_data_type;
+
+int _putchar(char)
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);

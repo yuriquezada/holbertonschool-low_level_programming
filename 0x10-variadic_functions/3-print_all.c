78d7ad6b058a58186c30fe097de6b0ea4551dd6f
yuriquezada <quezadat.yuri@gmail.com>
2021-07-22 21:23:57 -0700

    Create 3-print_all.c file

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,92 @@
+#include "variadic_functions.h"
+#include <stdarg.h>
+#include <stdio.h>
+
+/**
+ * print_c - print a char
+ * @c: char to print
+ *
+ * Return: void
+ */
+void print_c(va_list c)
+{
+	printf("%c", va_arg(c, int));
+}
+
+/**
+ * print_s - print a string
+ * @s: string to print
+ *
+ * Return: void
+ */
+void print_s(va_list s)
+{
+	char *str = va_arg(s, char *);
+
+	if (str == NULL)
+		str = "(nil)";
+	printf("%s", str);
+}
+
+/**
+ * print_i - print an int
+ * @i: int to print
+ *
+ * Return: void
+ */
+void print_i(va_list i)
+{
+	printf("%d", va_arg(i, int));
+}
+
+/**
+ * print_f - print a float
+ * @f: float to print
+ *
+ * Return: void
+ */
+void print_f(va_list f)
+{
+	printf("%f", va_arg(f, double));
+}
+
+/**
+ * prin_all - print anything
+ * @format: string containing the data types of the arguments
+ *
+ * Return: void
+ */
+void print_all(const char * const format, ...)
+{
+	unsigned int i, j;
+	print_based_data_type alternatives[] = {
+		{"c", print_c},
+		{"s", print_s},
+		{"i", print_i},
+		{"f", print_f},
+		{NULL, NULL}
+	};
+	va_list type_data;
+	char *separator = "";
+
+	va_start(type_data, format);
+	i = 0;
+	while (format && format[i])
+	{
+		j = 0;
+		while (alternatives[j].c != NULL)
+		{
+			if (*(alternatives[j].c) == format[i])
+			{
+				printf("%s", separator);
+				p[j].f(type_data);
+				separator = ", ";
+				break;
+			}
+			j++;
+		}
+		i++;
+	}
+	va_end(type_data);
+	printf("\n");
+}

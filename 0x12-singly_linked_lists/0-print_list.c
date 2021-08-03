1e8bd674acef9f5632d0c2962d4de833e6e868e6
Yuri Quezada <45071280+yuriquezada@users.noreply.github.com>
2021-08-02 23:06:20 -0500

    Update 0-print_list.c

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -10,10 +10,11 @@ size_t print_list(const list_t *h)
 {
 	size_t i;
 
-	for (i = 0; h; i++)
-	{
-		printf("[%d] %s\n", h->len, h->str);
-		h = h->next;
-	}
+	if (h)
+		for (i = 0; h; i++)
+		{
+			printf("[%d] %s\n", h->len, h->str);
+			h = h->next;
+		}
 	return (i);
 }

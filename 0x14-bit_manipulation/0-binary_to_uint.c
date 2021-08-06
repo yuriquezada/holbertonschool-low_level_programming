e735f78a0140ea49c94a915932a111d61c3566fa
yuriquezada <quezadat.yuri@gmail.com>
2021-08-05 20:07:32 -0700

    Use '<<' operator for the function

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -10,21 +10,19 @@
  */
 unsigned int binary_to_uint(const char *b)
 {
-	int i, len;
-	double sum = 0, pow_number = 0;
+	int i;
 	unsigned int decimal;
 
+	decimal = 0;
 	if (!b)
 		return (0);
-	len = strlen(b);
-	for (i = len - 1; i >= 0; i--)
+	for (i = 0; b[i]; i++)
 	{
 		if (b[i] != '0' && b[i] != '1')
 			return (0);
+		decimal <<= 1;
 		if (b[i] == '1')
-			sum += pow(2, pow_number);
-		pow_number++;
+			decimal += 1;
 	}
-	decimal = (unsigned int)(sum);
 	return (decimal);
 }

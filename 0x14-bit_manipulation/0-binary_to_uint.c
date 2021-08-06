b78e021ec9ea1bc78a8db3e6f2d611d64a21243d
yuriquezada <quezadat.yuri@gmail.com>
2021-08-05 19:18:45 -0700

    Create 0-binary_to_uint.c file

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+
+/**
+ * binary_to_uint - convert a binary number to an unsigned int
+ *
+ * Return: converted number, or 0
+ */
+unsigned int binary_to_uint(const char *b)
+{
+	int i, len;
+	double sum = 0, pow_number = 0;
+	unsigned int decimal;
+	len = strlen(b);
+	for (i = len - 1; i >= 0; i--)
+	{
+		if (!b)
+			return (0);
+		if (b[i] != '0' && b[i] != '1')
+			return (0);
+		if (b[i] == '1')
+			sum += pow(2, pow_number);
+		pow_number++;
+	}
+	decimal = (unsigned int)(sum);
+	return (decimal);
+}

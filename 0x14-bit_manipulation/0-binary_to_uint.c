371bcdef0ac0cb0150b4da2e4f5e70027e18d813
Yuri Quezada <45071280+yuriquezada@users.noreply.github.com>
2021-08-05 22:49:53 -0500

    Add argument in description

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -5,7 +5,7 @@
 
 /**
  * binary_to_uint - convert a binary number to an unsigned int
- *
+ * @b: pointer to a string of 0 and 1 chars
  * Return: converted number, or 0
  */
 unsigned int binary_to_uint(const char *b)

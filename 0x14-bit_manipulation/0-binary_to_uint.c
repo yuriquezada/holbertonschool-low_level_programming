ea18ea5459980ccb13c81b3d1ccf0fa1acc986d4
Yuri Quezada <45071280+yuriquezada@users.noreply.github.com>
2021-08-05 21:22:57 -0500

    Change location if b is NULL

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -13,11 +13,12 @@ unsigned int binary_to_uint(const char *b)
 	int i, len;
 	double sum = 0, pow_number = 0;
 	unsigned int decimal;
+
+	if (!b)
+		return (0);
 	len = strlen(b);
 	for (i = len - 1; i >= 0; i--)
 	{
-		if (!b)
-			return (0);
 		if (b[i] != '0' && b[i] != '1')
 			return (0);
 		if (b[i] == '1')
